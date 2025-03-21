dc740b6ad0a78d9d8f53d1fd536181c9fa9b7410
Reevs <onereevs@onereevss-MacBook-Pro.local>
2025-03-21 18:54:14 +0300

    feat: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,6 +9,12 @@ module.exports = {
     clean: true,
   },
   mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
